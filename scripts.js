08eadb29798790cfca91e5a029f51f66a48ba7bf
staffordgavin19 <77749110+staffordgavin19@users.noreply.github.com>
2021-04-05 13:08:37 -0500

    Update scripts

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
 const form = document.querySelector("form");
 const students = [];
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const newStudent = Object.fromEntries(new FormData(event.target));
 
   students.push(newStudent);
+});
+
+document.querySelector("button").addEventListener("click", () => {
+  console.log("hello");
 });
